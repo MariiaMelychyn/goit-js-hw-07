5dce1ac6a4205a0deaa8332e4446e6b02ae4c9a7
MariiaMelychyn <lv080191mmi75@gmail.com>
2021-07-02 23:43:42 +0300

    task-6

--- task-6.js
+++ task-6.js
@@ -1,1 +1,21 @@
 "use strict";
+
+let inputEl = document.getElementById('validation-input');
+
+let inputLength = inputEl.getAttribute('data-length');
+let InInputLength = parseInt(inputLength, 6);
+
+inputEl.oninput = function () {
+    if (inputEl.nodeValue.length === InInputLength){
+        inputEl.classList.remove('invalid');
+        inputEl.classList.add('valid');
+    }
+    if (inputEl.nodeValue.length === 0) {
+        inputEl.classList.remove('valid');
+        inputEl.classList.remove('invalid');
+    }
+    if (inputEl.nodeValue.length !== InInputLength && inputEl.value.length !== 0) {
+        inputEl.classList.add('invalid');
+    }
+};
+
