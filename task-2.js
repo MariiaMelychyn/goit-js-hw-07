56d138d528187fff913c819a6c77de39ffd2b6b6
MariiaMelychyn <lv080191mmi75@gmail.com>
2021-07-02 18:51:20 +0300

    task-2

--- task-2.js
+++ task-2.js
@@ -8,3 +8,10 @@ const ingredients = [
     'Зелень',
     'Приправы',
   ];
+
+  const ulEl = document.querySelector('#ingredients');
+
+  const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
+  
+  ulEl.innerHTML = list;
+  
