74ac22e9d07471dd27e1fa96c784716392462640
MariiaMelychyn <lv080191mmi75@gmail.com>
2021-07-02 23:06:23 +0300

    task-5

--- task-5.js
+++ task-5.js
@@ -1,1 +1,13 @@
 "use strict";
+
+let inputEl = document.getElementById("name-input");
+let nameOutput = document.getElementById("name-output");
+
+inputEl.oninput = function () {
+    if (inputEl.value === ' ') {
+        nameOutput.innerHTML = 'незнакомец';
+
+    } else {
+        nameOutput.innerHTML = inputEl.value;
+    }
+}
