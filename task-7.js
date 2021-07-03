424ba251dfe3d0e33388251fdf2ae3a411ba0f7e
MariiaMelychyn <lv080191mmi75@gmail.com>
2021-07-03 21:21:51 +0300

    task-7

--- task-7.js
+++ task-7.js
@@ -1,1 +1,10 @@
 "use strict";
+
+
+let controlEl = document.getElementById("font-size-control");
+let textEl = document.getElementById("text");
+
+ controlEl.addEventListener("input", (e) => {
+     const all = controlEl.nodeValue;
+     textEl.style.fontSize = size + "px";
+ });
